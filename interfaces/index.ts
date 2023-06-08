0651b27d29708d71eba7859e5181cd6c3987649b
Leandro <leandrom.roldan@gmail.com>
2023-06-08 15:50:00 -0300

    delete unused interface

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,9 +1,3 @@
-import { JwtPayload } from 'jsonwebtoken'
-
 export interface tokenObject {
     userId: string
-}
-export interface decodedTokenObject extends JwtPayload {
-    userId: string
-    iat: number
 }
